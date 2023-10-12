fc63d218ea1e65378bff2d6161649c880750a8e8
bhavitha03 <104689183+bhavitha03@users.noreply.github.com>
2023-10-12 13:46:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -71,3 +71,20 @@ if st.button("Predict"):
         else:
             st.write("Predicted: Not Depressed")
 
+# Add background image
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+            background-size: cover
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+add_bg_from_local('imp.jpg')
